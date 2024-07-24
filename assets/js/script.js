cae55cb4f82829218a73a4d8824617db91ad333f
Nurlana <nurlana@cubics.technology>
2024-07-24 15:16:45 +0400

    new pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionSpans = document.querySelectorAll('.accordion_top span');
 
+    accordionSpans.forEach(span => {
+        span.addEventListener('click', function (e) {
+            const top = span.parentElement;
+            const body = top.nextElementSibling;
+            top.classList.toggle('active');
+            if (body.style.display === 'block') {
+                body.style.display = 'none';
+            } else {
+                body.style.display = 'block';
+            }
+            e.stopPropagation(); // Prevent the event from bubbling up
+        });
+    });
+});
 // header slider
 const swiper = new Swiper('.swiper.swiper_header', {
     // Optional parameters
